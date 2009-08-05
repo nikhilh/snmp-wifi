119e66f7106f5a4cc696773d2abd7db7f0cf4012
KK Yap <ykk@kkpc.Stanford.EDU>
2009-08-04 20:22:26 -0700

    A central HACK for hard coding

---
+++ ap-subagents/snmpsys.h
@@ -0,0 +1,12 @@
+#ifndef SNMP_SYSTEM_VAR_H
+#define SNMP_SYSTEM_VAR_H
+
+/** This header file is created to "lessen" the hardcoding pains
+ * of these subagents.  By adding so, I am trying to make my life
+ * easier, which does not mean I condone such coding practices.
+ */
+
+#define SNMP_TRAP_HOST "openroads.controller"
+#define SNMP_TRAP_BIN "/usr/bin/snmptrap"
+
+#endif
