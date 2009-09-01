ae2369dae91e8c4ef385b73bf08ab209f8bd287c
KK Yap <ykk@kkpc.Stanford.EDU>
2009-09-01 14:24:59 -0700

    Ensure mib naming

--- copy-all2ap.sh
+++ copy-all2ap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp config-files/POMI-MOBILITY-MIB.txt $1:/usr/share/snmp/mibs/.
-ssh $1 "echo POMI-MOBILITY-MIB.txt >> /usr/share/snmp/mibs/.index"
+ssh $1 "echo POMI-MOBILITY-MIB POMI-MOBILITY-MIB.txt >> /usr/share/snmp/mibs/.index"
 scp config-files/snmpd.conf ap-subagents/*_subagent $1:/etc/snmp/.
 scp config-files/openroads $1:/etc/init.d/.
 ssh $1 "ln -s /etc/init.d/openroads /etc/rc2.d/S99openroads"
