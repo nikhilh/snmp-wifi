ae46bcad2b20749a7af9a6f2f68060835183060a
Nikhil Handigol <nikhilh@stanford.edu>
2009-11-23 00:42:54 -0800

    some cleanup

--- ap-subagents/util.h
+++ ap-subagents/util.h
@@ -7,31 +7,4 @@
 #define INTERFACE "ath0"
 #define DATAPATH "nl:0"
 
-/*void getargs(char *argstr, char **ret; int *numargs)
-{
-    char *token;
-    *numargs = 0;
-    int i;
-    for(token = strtok(argstr, " "); token != NULL; token = strtok(NULL, " ")) {
-        *numargs++;
-    }
-    ret = malloc(sizeof(char*)*(*numargs));
-    for(token = strtok(argstr, " "), i = 0; token != NULL; token = strtok(NULL, " "), i++) {
-        (char *)(*(ret+i)) = malloc(sizeof(char)*(strlen(token)+1));
-        strncpy(*(ret+i), token, strlen(token));
-        (*(ret+i))[strlen(token)] = '\0';
-    }
-    return ret;
-}
-
-
-void freeargs(char **args, int numargs)
-{
-    int i = 0;
-    for(i = 0; i < numargs; i++) {
-        free((char*)(*(args + i)));
-    }
-    free(args);
-}*/
-
 #endif //UTIL_H
